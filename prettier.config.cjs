23bedddd73528ae533a1aa90d3e65ebdde25bfa4
Alejandro Nanez <git@alejandro.dev>
2024-07-22 19:50:15 -0500

    prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,20 +3,16 @@ module.exports = {
   // i am just using the standard config, change if you need something else
   ...require('prettier-config-standard'),
   pluginSearchDirs: [__dirname],
-  plugins: [
-    'prettier-plugin-astro',
-    'prettier-plugin-tailwindcss'
-  ],
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
   overrides: [
     {
       files: '*.astro',
       options: {
         parser: 'astro'
       }
-    },
+    }
   ],
-  useTabs: true,
+  useTabs: false,
   singleQuote: true,
-  trailingComma: 'none',
-  printWidth: 100
+  trailingComma: 'none'
 }
